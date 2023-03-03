3a57318370ac4d364e26eda1aa8905f9f2b1ae83
Noah Weninger <nweninge@ualberta.ca>
2023-03-02 20:58:24 -0500

    instances with small number of items but very high capacity

---
+++ instances/largecap/gen_new.py
@@ -0,0 +1,43 @@
+from random import randint, shuffle
+import math
+
+def uniform_array(n, lo, hi):
+    return [randint(lo, hi) for i in range(n)]
+
+def uniform_array_nodup(n, lo, hi, skip=1):
+    assert(n <= (hi-lo+skip)//skip)
+    a = list(range(lo, hi+1, skip))
+    shuffle(a)
+    return a[:n]
+
+def add(x, y):
+    return [z+y for z in x]
+
+num_inst = 0
+def write_instance(n, p, cu, cl, wu, wl, meta={"group":"LargeCap"}):
+    global num_inst
+    with open("largecap{}.ki".format(num_inst), "w") as f:
+        f.write("{}\n{}\n{}\n".format(n,cl,cu))
+        f.write("{}\n".format(" ".join(map(str, wl))))
+        f.write("{}\n".format(" ".join(map(str, wu))))
+        f.write("{}\n".format(" ".join(map(str, p))))
+        for key in meta:
+            f.write("{} {}\n".format(key, meta[key]))
+        num_inst += 1
+
+sizes = [10, 20, 30]
+n_of_each = 1
+R = [10000, 100000, 1000000]
+
+for j in range(len(sizes)):
+    n = sizes[j]
+    for i in range(n_of_each):
+        for r in R:
+            for ins in range(1, 11):
+                p = uniform_array(n, 1, r)
+                wu = uniform_array(n, 1, r)
+                wl = uniform_array(n, 1, r)
+                cl = math.ceil(ins/22*sum(wl))
+                write_instance(n, p, max(max(wu), cl+randint(-10,10)),
+                        max(max(wl),cl), wu, wl,
+                        {"group":"LargeCap", "class":"uc", "ins":ins/2})
--- run_tests.py
+++ run_tests.py
@@ -179,13 +179,16 @@ all_solvers = [
     Solver("build/bkpsolver", ["comb", "-l0", "-p", "-q", "-j1"], EXACT),
     Solver("build/bkpsolver", ["comb", "-l0", "-p", "-q", "-j4"], EXACT),
     Solver("build/bkpsolver", ["comb", "-l0", "-p", "-q", "-j16"], EXACT),
-    #Solver("build/bkpsolver", ["dcs", "-a2", "-b2", "-g5", "-d20", "-m1000", "-o5", "-q"], EXACT)
+    Solver("build/bkpsolver", ["dcs", "-a2", "-b2", "-g5", "-d20", "-m1000", "-o5", "-q"], EXACT)
 ]
 
 tests = get_all_tests()
 new_tests = list(filter(lambda t: t.group in ["New", None], tests))
 lit_tests = list(filter(lambda t: t.group in ["CCLW", "DCS", "FMS", "TRS", "DeNegre"], tests))
+largecap_tests = list(filter(lambda t: t.group in ["LargeCap"], tests))
 os.makedirs(output_dir, exist_ok=True)
 with open(output_dir + "/all_results", "a") as f:
     run_tests(all_solvers, lit_tests, f, timeout=3600)
     run_tests(all_solvers, new_tests, f, timeout=900)
+    run_tests(all_solvers, largecap_tests, f, timeout=900)
+
